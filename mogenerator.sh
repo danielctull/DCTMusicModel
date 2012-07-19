c8fdc99b7d9127906b0a39a5512c7b75890a0f62
Daniel Tull <dt@danieltull.co.uk>
2012-07-19 14:59:39 +0100

    Add bundle and rename entities to DCT

---
+++ mogenerator.sh
@@ -0,0 +1,1 @@
+mogenerator -m DCTMusicModelBundle/DCTMusicModel.xcdatamodeld/DCTMusicModel.xcdatamodel -M DCTMusicModel/Model/Generated -H /DCTMusicModel/Model --template-var arc=true --template-var frc=true

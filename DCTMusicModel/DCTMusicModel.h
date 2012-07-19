6022e7d370ba52d4e7bbd1a85db0cd87141be3db
Daniel Tull <dt@danieltull.co.uk>
2012-07-19 14:34:06 +0100

    Add DCTMusicModel static library target

---
+++ DCTMusicModel/DCTMusicModel.h
@@ -0,0 +1,13 @@
+//
+//  DCTMusicModel.h
+//  DCTMusicModel
+//
+//  Created by Daniel Tull on 19.07.2012.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCTMusicModel : NSObject
+
+@end
---
+++ DCTMusicModel/DCTMusicModel.m
@@ -0,0 +1,13 @@
+//
+//  DCTMusicModel.m
+//  DCTMusicModel
+//
+//  Created by Daniel Tull on 19.07.2012.
+//
+//
+
+#import "DCTMusicModel.h"
+
+@implementation DCTMusicModel
+
+@end
